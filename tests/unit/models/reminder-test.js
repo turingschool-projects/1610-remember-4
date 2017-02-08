baf05ad485235a547c02e0a6c5f482a8555d83b7
Devin Beliveau <devinmarieb@gmail.com>
2017-02-08 15:38:16 -0700

    Delete unneccessary tests

--- tests/unit/models/reminder-test.js
+++ tests/unit/models/reminder-test.js
@@ -4,9 +4,3 @@ moduleForModel('reminder', 'Unit | Model | reminder', {
   // Specify the other units that are required for this test.
   needs: []
 });
-
-test('it exists', function(assert) {
-  let model = this.subject();
-  // let store = this.store();
-  assert.ok(!!model);
-});
