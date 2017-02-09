16c50cde59b297103f8dc61cfef4f9983147197b
Devin Beliveau <devinmarieb@gmail.com>
2017-02-09 16:39:50 -0700

    Fix tests to eliminate Ember.$

--- tests/acceptance/reminder-list-test.js
+++ tests/acceptance/reminder-list-test.js
@@ -14,7 +14,7 @@ test('viewing the homepage', function(assert) {
 
   andThen(function() {
     assert.equal(currentURL(), '/reminders');
-    assert.equal(Ember.$('.spec-reminder-item').length, 5);
+    assert.equal(find('.spec-reminder-item').length, 5);
   });
 });
 
@@ -26,15 +26,15 @@ test('clicking on an individual item', function(assert) {
 
   andThen(function() {
     assert.equal(currentURL(), '/reminders/1');
-    assert.equal(Ember.$('.spec-reminder-item:first').text().trim(), Ember.$('.spec-reminder-title').text().trim());
+    assert.equal(find('.spec-reminder-item:first').text().trim(), find('.spec-reminder-title').text().trim());
   });
 });
 
 test('clicking on an individual item adds a class of active', function(assert) {
-    server.createList('reminder', 5);
+  server.createList('reminder', 5);
 
-    visit('/');
-    click('.spec-reminder-item:first');
+  visit('/');
+  click('.spec-reminder-item:first');
 
   andThen(function() {
     assert.equal(find('.active').length, 1);
