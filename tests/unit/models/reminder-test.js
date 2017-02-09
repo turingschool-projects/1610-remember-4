8da8e5a4ee1f22a3ea24c6fdccd68e3f4f0ccac9
Devin Beliveau <devinmarieb@gmail.com>
2017-02-09 14:31:53 -0700

    Add model test for reminder title, data and notes

--- tests/unit/models/reminder-test.js
+++ tests/unit/models/reminder-test.js
@@ -4,3 +4,10 @@ moduleForModel('reminder', 'Unit | Model | reminder', {
   // Specify the other units that are required for this test.
   needs: []
 });
+
+test('each reminder should have a title, date and note', function(assert) {
+  let reminders = this.subject({title: 'some lorem ipsum', date: 'Tue Feb 07 2017 02:43:02 GMT-0700 (MST)', notes: 'some more lorem ipsum'});
+  assert.equal(reminders.get('title'), 'some lorem ipsum', 'this is the reminder title')
+  assert.equal(reminders.get('date'), 'Tue Feb 07 2017 02:43:02 GMT-0700 (MST)', 'this is the reminder date')
+  assert.equal(reminders.get('notes'), 'some more lorem ipsum', 'this is the reminder note')
+});
