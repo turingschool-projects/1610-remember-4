3c76f680efa13d079f37b35d9592b717656e95cd
Devin Beliveau <devinmarieb@gmail.com>
2017-02-09 15:11:42 -0700

    Add test to assert reminder title generation is same as link

--- tests/acceptance/reminder-list-test.js
+++ tests/acceptance/reminder-list-test.js
@@ -32,10 +32,22 @@ test('clicking on an individual item', function(assert) {
 
 test('clicking on an individual item adds a class of active', function(assert) {
     server.createList('reminder', 5);
+
     visit('/');
     click('.spec-reminder-item:first');
 
   andThen(function() {
     assert.equal(find('.active').length, 1);
+  });
+});
+
+test('clicking on an individual reminder renders the correct title element', function(assert) {
+  server.createList('reminder', 5);
+
+  visit('/');
+  click('.spec-reminder-item:first');
+
+  andThen(function() {
+    assert.equal(find('h2').text().trim(), find('.spec-reminder-item:first').text().trim());
   });
 });
