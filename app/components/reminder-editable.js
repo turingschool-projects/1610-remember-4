d2e1ae673956bbcd610b38d1040a54c178f1679d
Mike Ziccardi <mikeziccardi@gmail.com>
2017-02-15 12:38:21 -0700

    user is able to delete a reminder

--- app/components/reminder-editable.js
+++ app/components/reminder-editable.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-});
+  store: Ember.inject.service(),
+
+  actions: {
+    remove(){
+      console.log('hi');
+      this.get('store').findRecord('reminder', this.model.id).then(reminder =>{
+        reminder.destroyRecord()
+      })
+    }
+  }
+})
